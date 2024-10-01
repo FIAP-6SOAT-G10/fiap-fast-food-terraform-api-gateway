a64013605502b9e0b2af9a08ae4cb0b6d750be63
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-09-30 22:08:20 -0300

    fix: Adjust API Gateway description to Terraform

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,6 +1,6 @@
 resource "aws_api_gateway_rest_api" "api-gateway" {
   name        = "fiap-fast-food-api-gateway"
-  description = "Main AWS API Gateway to Tech Challenge's phase 3"
+  description = "Main AWS API Gateway to Tech Challenge's fase 3"
 }
 
 provider "aws" {
