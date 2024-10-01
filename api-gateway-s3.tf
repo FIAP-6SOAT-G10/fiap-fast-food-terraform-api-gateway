595329520ed2031c1e54fe8f2eb34311d5eb60e7
Rafael de Mesquita Moura <rafael.mesquita.moura@gmail.com>
2024-10-01 16:14:00 -0300

    Revert "feat: Include TF state files in S3 Bucket"

--- api-gateway-s3.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "api_gateway_s3" {
-  bucket = "fiapfastfood"
-
-  tags = {
-    Name        = "Fiap Fast Food S3 Bucket"
-  }
-}
--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "fiapfastfood"
-    key = "apigateway/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
